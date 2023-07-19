9bf83a6904c2b7f93f9d72e0290c765f5c570591
Mike <mike@Mikes-MacBook-Pro.local>
2023-07-20 01:01:04 +0700

    comment unuse

--- db/database.go
+++ db/database.go
@@ -15,16 +15,16 @@ var DB *gorm.DB
 func ConnectToDB() {
 	config.LoadEnvVariables()
 	dbREMOTE_DB_URL := os.Getenv("REMOTE_DB_URL")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
+	// dbHost := os.Getenv("DB_HOST")
+	// dbPort := os.Getenv("DB_PORT")
+	// dbUser := os.Getenv("DB_USER")
+	// dbPassword := os.Getenv("DB_PASSWORD")
+	// dbName := os.Getenv("DB_NAME")
 
-	dataSourceName1 := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbName)
+	// dataSourceName1 := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbName)
 	dataSourceName := dbREMOTE_DB_URL
 	fmt.Println("dataSourceName: ", dataSourceName)
-	fmt.Println("dataSourceName1: ", dataSourceName1)
+	// fmt.Println("dataSourceName1: ", dataSourceName1)
 
 	var err error
 	DB, err = gorm.Open(mysql.Open(dataSourceName), &gorm.Config{})
