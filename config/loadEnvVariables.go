d337b21b1adc389c1de8fdae86e46056350f8d7f
maiyzaza <maiy27982@gmail.com>
2023-07-20 17:02:27 +0000

    Update loadEnvVariables.go

--- config/loadEnvVariables.go
+++ config/loadEnvVariables.go
@@ -9,12 +9,29 @@ import (
 
 var ENV *string
 
-func LoadEnvVariables() {
-	if err := godotenv.Load(".env"); err != nil {
-		fmt.Println("No environment file found")
-	}
+// func LoadEnvVariables() {
+// 	if err := godotenv.Load(".env"); err != nil {
+// 		fmt.Println("No environment file found")
+// 	}
 
-	dbHost := os.Getenv("REMOTE_DB_URL")
-	ENV = &dbHost // Assuming ENV is used to store the DB_HOST value
-	fmt.Printf("This environment state \"DB_HOST=%s\" is still running...\n", *ENV)
+// 	dbHost := os.Getenv("REMOTE_DB_URL")
+// 	ENV = &dbHost // Assuming ENV is used to store the DB_HOST value
+// 	fmt.Printf("This environment state \"DB_HOST=%s\" is still running...\n", *ENV)
+// }
+func LoadEnvVariables() {
+    // Assuming ENV is used to store the DB_HOST value
+    envFilePath := os.Getenv("ENV_FILE_PATH")
+    if envFilePath == "" {
+        fmt.Println("ENV_FILE_PATH environment variable not set")
+        return
+    }
+
+    if err := godotenv.Load(envFilePath); err != nil {
+        fmt.Println("Error loading environment file:", err)
+        return
+    }
+
+    dbHost := os.Getenv("REMOTE_DB_URL")
+    ENV = &dbHost
+    fmt.Printf("This environment state \"DB_HOST=%s\" is still running...\n", *ENV)
 }
