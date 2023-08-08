525fb9ada5c526ce0cb1eb4a575b5a5c2fa2f539
Mike <mike@Mikes-MacBook-Pro.local>
2023-08-08 11:51:02 +0700

    change database.go

--- db/database.go
+++ db/database.go
@@ -23,8 +23,8 @@ func ConnectToDB() {
 	// dbName := os.Getenv("DB_NAME")
 
 	// dataSourceName1 := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbName)
-	// dataSourceName := "doadmin:AVNS_nnRbPmxqVlOi3mYo5LT@tcp(db-mysql-sgp1-88785-do-user-14398363-0.b.db.ondigitalocean.com:25060)/defaultdb?charset=utf8mb4&parseTime=True&loc=Local"
-	dataSourceName := "root:my-secret-pw@tcp(localhost:1433)/PattayaAvenueProperty?charset=utf8mb4&parseTime=True&loc=Local"
+	dataSourceName := "doadmin:AVNS_nnRbPmxqVlOi3mYo5LT@tcp(db-mysql-sgp1-88785-do-user-14398363-0.b.db.ondigitalocean.com:25060)/PattayaAvenueProperty_Staging?charset=utf8mb4&parseTime=True&loc=Local"
+	// dataSourceName := "root:my-secret-pw@tcp(localhost:1433)/PattayaAvenueProperty?charset=utf8mb4&parseTime=True&loc=Local"
 	fmt.Println("dataSourceName: ", dataSourceName)
 	// fmt.Println("dataSourceName1: ", dataSourceName1)
 
